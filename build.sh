d85e392e574b82bb1d642f37bd0023e673c43b86
kms1212 <kms1212g@gmail.com>
2026-02-04 21:04:55 +0900

    stabilized gcc behavior after installation and added github workflow

--- build.sh
+++ build.sh
@@ -2,42 +2,117 @@
 
 set -eu
 
+OSNAME=$(uname -s)
+ROOT="$PWD"
+
+ARCHIVE_NAME="folisdk-0.0.1.tar.gz"
+TARGET=x86_64-strata-folios
 ZLIB_VER="1.3.1"
+PREFIX=
+SYSROOT=
+PARALLEL=
 
-PWD=$(pwd)
-NPROC=$(sysctl -n hw.ncpu)
-PARALLEL=$(($NPROC - 1))
+GETOPT_OUTPUT=$(getopt -o "hp:t:z:s:j:" --long "help,prefix:,target:,zlib-version:,sysroot:,jobs:" --name "$(basename "$0")" -- "$@")
 
-export PREFIX="$PWD/build/pkgroot"
-export TARGET=x86_64-strata-folios
-export SYSROOT="$PREFIX"
-export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+if [ $? != 0 ]; then
+    exit 1
+fi
 
-export PATH="$PREFIX/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+eval set -- "$GETOPT_OUTPUT"
+
+while :; do
+    case $1 in
+        -h | --help)
+            echo "Usage: $(basename "$0") [options]"
+            echo "Options:"
+            echo "  -p, --prefix <path>    Set the prefix directory (default: $PREFIX)"
+            echo "  -t, --target <target>  Set the target architecture (default: $TARGET)"
+            echo "  -z, --zlib-version <version>  Set the zlib version (default: $ZLIB_VER)"
+            echo "  -s, --sysroot <path>   Set the sysroot directory (default: $SYSROOT)"
+            echo "  -j, --jobs <number>    Set the number of jobs (default: $PARALLEL)"
+            exit 0
+            ;;
+        -p | --prefix)
+            PREFIX="$2"
+            shift 2
+            ;;
+        -t | --target)
+            TARGET="$2"
+            shift 2
+            ;;
+        -z | --zlib-version)
+            ZLIB_VER="$2"
+            shift 2
+            ;;
+        -s | --sysroot)
+            SYSROOT="$2"
+            shift 2
+            ;;
+        -j | --jobs)
+            PARALLEL="$2"
+            shift 2
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$PREFIX" ]; then
+    if [ "$OSNAME" == "Darwin" ]; then
+        PREFIX="/opt/homebrew/opt/folisdk"
+    else
+        PREFIX="/opt/folisdk"
+    fi
+fi
+
+if [ -z "$SYSROOT" ]; then
+    SYSROOT="$PREFIX/$TARGET/sysroot"
+fi
+
+if [ -z "$PARALLEL" ]; then
+    if [ "$OSNAME" == "Darwin" ]; then
+        PARALLEL=$(($(sysctl -n hw.ncpu) - 1))
+    else
+        PARALLEL=$(($(nproc) - 1))
+    fi
+fi
+
+if [ "$OSNAME" == "Darwin" ]; then
+    SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+    export SDKROOT
+fi
+
+BUILDDIR="$PWD/build/pkgroot"
+
+export PATH="$PREFIX/bin:$BUILDDIR/$PREFIX/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 unset LD_LIBRARY_PATH
 unset DYLD_LIBRARY_PATH
 unset C_INCLUDE_PATH
 unset CPLUS_INCLUDE_PATH
 unset LIBRARY_PATH
 
-export CPPFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+export CPPFLAGS="-I$BUILDDIR/$PREFIX/include"
+export LDFLAGS="-L$BUILDDIR/$PREFIX/lib"
 
-mkdir -p "$SYSROOT"
+mkdir -p "$BUILDDIR"
+mkdir -p "$BUILDDIR/$SYSROOT"
 
 configure_zlib() {
     cd build
 
-    if [ ! -f "zlib-${ZLIB_VER}.tar.gz" ]; then
-
-    ln -s ../pkgroot/usr/lib/crt1.o ../pkgroot/usr/lib/crt0.o
-    ln -s ../pkgroot/usr/lib/crt1.o ../pkgroot/lib/crt0.o
-        curl -O https://zlib.net/zlib-${ZLIB_VER}.tar.gz
+    if [ ! -f "zlib-$ZLIB_VER.tar.gz" ]; then
+        curl -O "https://zlib.net/zlib-$ZLIB_VER.tar.gz"
     fi
 
     rm -rf zlib
-    tar -xzf zlib-${ZLIB_VER}.tar.gz
-    mv zlib-${ZLIB_VER} zlib
+    tar -xzf "zlib-$ZLIB_VER.tar.gz"
+    mv "zlib-$ZLIB_VER" zlib
 
     cd zlib
 
@@ -49,8 +124,8 @@ configure_zlib() {
 make_zlib() {
     cd build/zlib
 
-    make -j$PARALLEL
-    make install
+    make -j"$PARALLEL"
+    make install DESTDIR="$BUILDDIR"
 
     cd ../..
 }
@@ -62,8 +137,9 @@ configure_binutils() {
     cd binutils
 
     ../../binutils-strata/configure \
-        --target=$TARGET \
+        --target="$TARGET" \
         --prefix="$PREFIX" \
+        --with-build-sysroot="$BUILDDIR/$SYSROOT" \
         --with-sysroot="$SYSROOT" \
         --disable-nls \
         --disable-werror \
@@ -76,8 +152,8 @@ configure_binutils() {
 make_binutils() {
     cd build/binutils
 
-    make -j$PARALLEL MAKEINFO=true
-    make install MAKEINFO=true
+    make -j"$PARALLEL"
+    make install DESTDIR="$BUILDDIR"
 
     cd ../..
 }
@@ -89,7 +165,7 @@ configure_gcc_pass1() {
     cd gcc-pass1
 
     ../../gcc-strata/configure \
-        --target=$TARGET \
+        --target="$TARGET" \
         --prefix="$PREFIX" \
         --with-sysroot="$SYSROOT" \
         --with-native-system-header-dir="/usr/include" \
@@ -116,22 +192,22 @@ make_gcc_pass1() {
 
     cd build/gcc-pass1
 
-    make -j$PARALLEL all-gcc MAKEINFO=true
-    make -j$PARALLEL all-target-libgcc MAKEINFO=true
-    make install-gcc MAKEINFO=true
-    make install-target-libgcc MAKEINFO=true
+    make -j"$PARALLEL" all-gcc
+    make -j"$PARALLEL" all-target-libgcc
+    make install-gcc DESTDIR="$BUILDDIR"
+    make install-target-libgcc DESTDIR="$BUILDDIR"
 
-    GCC_LIB_PATH=$("$PREFIX/bin/$TARGET-gcc" -print-libgcc-file-name | xargs dirname)
+    GCC_LIB_PATH=$("$BUILDDIR/$PREFIX/bin/$TARGET-gcc" -print-libgcc-file-name | xargs dirname)
     GCC_INTERNAL_HEADERS_PATH="$GCC_LIB_PATH/include"
     GCC_INTERNAL_FIXED_HEADERS_PATH="$GCC_LIB_PATH/include-fixed"
 
-    mkdir -p "$SYSROOT/usr/include"
+    mkdir -p "$BUILDDIR/$SYSROOT/usr/include"
 
-    cp -r "$GCC_INTERNAL_HEADERS_PATH"/* "$SYSROOT/usr/include"
+    cp -r "$GCC_INTERNAL_HEADERS_PATH"/* "$BUILDDIR/$SYSROOT/usr/include"
     if [ -d "$GCC_INTERNAL_FIXED_HEADERS_PATH" ]; then
-        cp -r "$GCC_INTERNAL_FIXED_HEADERS_PATH"/* "$SYSROOT/usr/include"
+        cp -r "$GCC_INTERNAL_FIXED_HEADERS_PATH"/* "$BUILDDIR/$SYSROOT/usr/include"
     fi
-    cp -r "../../gcc-strata/gcc/ginclude/stdint-gcc.h" "$SYSROOT/usr/include/stdint.h"
+    cp -r "../../gcc-strata/gcc/ginclude/stdint-gcc.h" "$BUILDDIR/$SYSROOT/usr/include/stdint.h"
 
     cd ../..
 }
@@ -143,10 +219,9 @@ configure_musl_pass1() {
     cd musl-pass1
 
     CROSS_COMPILE="$TARGET-" \
-    CFLAGS="-fPIC" \
     ../../musl-strata/configure \
-        --target=$TARGET \
-        --prefix="$PREFIX/usr" \
+        --target="$TARGET" \
+        --prefix="$SYSROOT/usr" \
         --disable-shared \
         --disable-gcc-wrapper
 
@@ -158,14 +233,13 @@ make_musl_pass1() {
 
     make install-headers
 
-    make -j$PARALLEL
+    make -j"$PARALLEL"
 
-    make install
+    make install DESTDIR="$BUILDDIR"
 
-    ln -s crt1.o "$PREFIX/usr/lib/crt0.o"
-    ln -s ../usr/lib/crt1.o "$PREFIX/lib/crt0.o"
+    ln -s crt1.o "$BUILDDIR/$SYSROOT/usr/lib/crt0.o"
 
-    echo "GROUP ( libc.a )" > "$PREFIX/usr/lib/libc.so"
+    echo "GROUP ( libc.a )" > "$BUILDDIR/$SYSROOT/usr/lib/libc.so"
 
     cd ../..
 }
@@ -177,8 +251,9 @@ configure_gcc_pass2() {
     cd gcc-pass2
 
     ../../gcc-strata/configure \
-        --target=$TARGET \
+        --target="$TARGET" \
         --prefix="$PREFIX" \
+        --with-build-sysroot="$BUILDDIR/$SYSROOT" \
         --with-sysroot="$SYSROOT" \
         --with-native-system-header-dir="/usr/include" \
         --with-system-zlib \
@@ -201,13 +276,13 @@ configure_gcc_pass2() {
 make_gcc_pass2() {
     cd build/gcc-pass2
 
-    make -j$PARALLEL all-gcc MAKEINFO=true
-    make -j$PARALLEL all-target-libgcc MAKEINFO=true
-    make install-gcc MAKEINFO=true
-    make install-target-libgcc MAKEINFO=true
+    make -j"$PARALLEL" all-gcc
+    make -j"$PARALLEL" all-target-libgcc
+    make install-gcc DESTDIR="$BUILDDIR"
+    make install-target-libgcc DESTDIR="$BUILDDIR"
 
-    make -j$PARALLEL all-target-libstdc++-v3 MAKEINFO=true
-    make install-target-libstdc++-v3 MAKEINFO=true
+    make -j"$PARALLEL" all-target-libstdc++-v3
+    make install-target-libstdc++-v3 DESTDIR="$BUILDDIR"
     
     cd ../..
 }
@@ -220,10 +295,9 @@ configure_musl_pass2() {
     cd musl-pass2
 
     CROSS_COMPILE="$TARGET-" \
-    LDFLAGS="-no-pie" \
     ../../musl-strata/configure \
-        --target=$TARGET \
-        --prefix="$PREFIX/usr" \
+        --target="$TARGET" \
+        --prefix="$SYSROOT/usr" \
         --disable-gcc-wrapper
 
     cd ../..
@@ -234,69 +308,127 @@ make_musl_pass2() {
 
     make install-headers
 
-    make -j$PARALLEL
+    make -j"$PARALLEL"
 
-    make install
+    make install DESTDIR="$BUILDDIR"
+
+    rm -f "$BUILDDIR/lib/ld-musl-x86_64.so.1"
+    rmdir "$BUILDDIR/lib" || true
+
+    ln -sf "../$TARGET/sysroot/usr/lib/libc.so" "$BUILDDIR/$PREFIX/lib/ld-musl-x86_64.so.1"
 
     cd ../..
 }
 
-if [ ! -f "build/.configure-zlib.stamp" ]; then
+make_archive() {
+    cd build
+
+    cd "$BUILDDIR/$PREFIX"
+    tar -czvf "$ROOT/build/$ARCHIVE_NAME" .
+
+    cd "$ROOT"
+}
+
+if [ ! -f "build/.configure-zlib.stamp" ] || [ "$1" == "cfg-zlib" ]; then
+    if [ "$1" == "cfg-zlib" ]; then
+        shift
+    fi
     configure_zlib
     touch "build/.configure-zlib.stamp"
 fi
 
-if [ ! -f "build/.zlib.stamp" ]; then
+if [ ! -f "build/.zlib.stamp" ] || [ "$1" == "make-zlib" ]; then
+    if [ "$1" == "make-zlib" ]; then
+        shift
+    fi
     make_zlib
     touch "build/.zlib.stamp"
 fi
 
-if [ ! -f "build/.configure-binutils.stamp" ]; then
+if [ ! -f "build/.configure-binutils.stamp" ] || [ "$1" == "cfg-binutils" ]; then
+    if [ "$1" == "cfg-binutils" ]; then
+        shift
+    fi
     configure_binutils
     touch "build/.configure-binutils.stamp"
 fi
 
-if [ ! -f "build/.binutils.stamp" ]; then
+if [ ! -f "build/.binutils.stamp" ] || [ "$1" == "make-binutils" ]; then
+    if [ "$1" == "make-binutils" ]; then
+        shift
+    fi
     make_binutils
     touch "build/.binutils.stamp"
 fi
 
-if [ ! -f "build/.configure-gcc-pass1.stamp" ]; then
+if [ ! -f "build/.configure-gcc-pass1.stamp" ] || [ "$1" == "cfg-gcc-pass1" ]; then
+    if [ "$1" == "cfg-gcc-pass1" ]; then
+        shift
+    fi
     configure_gcc_pass1
     touch "build/.configure-gcc-pass1.stamp"
 fi
 
-if [ ! -f "build/.gcc-pass1.stamp" ]; then
+if [ ! -f "build/.gcc-pass1.stamp" ] || [ "$1" == "make-gcc-pass1" ]; then
+    if [ "$1" == "make-gcc-pass1" ]; then
+        shift
+    fi
     make_gcc_pass1
     touch "build/.gcc-pass1.stamp"
 fi
 
-if [ ! -f "build/.configure-musl-pass1.stamp" ]; then
+if [ ! -f "build/.configure-musl-pass1.stamp" ] || [ "$1" == "cfg-musl-pass1" ]; then
+    if [ "$1" == "cfg-musl-pass1" ]; then
+        shift
+    fi
     configure_musl_pass1
     touch "build/.configure-musl-pass1.stamp"
 fi
 
-if [ ! -f "build/.musl-pass1.stamp" ]; then
+if [ ! -f "build/.musl-pass1.stamp" ] || [ "$1" == "make-musl-pass1" ]; then
+    if [ "$1" == "make-musl-pass1" ]; then
+        shift
+    fi
     make_musl_pass1
     touch "build/.musl-pass1.stamp"
 fi
 
-if [ ! -f "build/.configure-gcc-pass2.stamp" ]; then
+if [ ! -f "build/.configure-gcc-pass2.stamp" ] || [ "$1" == "cfg-gcc-pass2" ]; then
+    if [ "$1" == "cfg-gcc-pass2" ]; then
+        shift
+    fi
     configure_gcc_pass2
     touch "build/.configure-gcc-pass2.stamp"
 fi
 
-if [ ! -f "build/.gcc-pass2.stamp" ]; then
+if [ ! -f "build/.gcc-pass2.stamp" ] || [ "$1" == "make-gcc-pass2" ]; then
+    if [ "$1" == "make-gcc-pass2" ]; then
+        shift
+    fi
     make_gcc_pass2
     touch "build/.gcc-pass2.stamp"
 fi
 
-if [ ! -f "build/.configure-musl-pass2.stamp" ]; then
+if [ ! -f "build/.configure-musl-pass2.stamp" ] || [ "$1" == "cfg-musl-pass2" ]; then
+    if [ "$1" == "cfg-musl-pass2" ]; then
+        shift
+    fi
     configure_musl_pass2
     touch "build/.configure-musl-pass2.stamp"
 fi
 
-if [ ! -f "build/.musl-pass2.stamp" ]; then
+if [ ! -f "build/.musl-pass2.stamp" ] || [ "$1" == "make-musl-pass2" ]; then
+    if [ "$1" == "make-musl-pass2" ]; then
+        shift
+    fi
     make_musl_pass2
     touch "build/.musl-pass2.stamp"
+fi
+
+if [ ! -f "build/.archive.stamp" ] || [ "$1" == "make-archive" ]; then
+    if [ "$1" == "make-archive" ]; then
+        shift
+    fi
+    make_archive
+    touch "build/.archive.stamp"
 fi
--- folisdk.rb
+++ folisdk.rb
@@ -1,10 +1,11 @@
 class Folisdk < Formula
-  desc "Cross-compiler toolchain for Strata OS"
+  desc "Cross-compiler toolchain for foliOS"
   homepage "https://github.com/kms1212/folisdk"
   
-  url "file://#{Dir.pwd}/folisdk-1.0.0.tar.gz"
+  url "file://#{Dir.pwd}/build/folisdk-0.0.1.tar.gz"
   
-  version "1.0.0"
+  version "0.0.1"
+  sha256 "a9db594d4edb7ff75545c1dfb600e5648ae1b673217d40bec07b389ea7de63cb"
   
   keg_only "it conflicts with standard gdb and binutils"
 
@@ -14,11 +15,11 @@ class Folisdk < Formula
 
   def caveats
     <<~EOS
-      To use the Folio OS toolchain, you need to add it to your PATH:
-
-        export PATH="#{opt_bin}:$PATH"
-
-      You can add this line to your ~/.zshrc or ~/.bash_profile.
+      Installed:
+      - gcc (C, C++)
+      - binutils
+      - gdb
+      - musl libc
     EOS
   end
 end
